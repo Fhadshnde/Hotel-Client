4b7912d631ba10b96804eae12562f350438c4117
Fhadshnde <Fhadshnde32@gmail.com>
2025-01-12 20:46:25 +0300

    edit

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get(`/api${url}`); // إضافة /api إلى بداية المسار
+        const res = await axios.get(url); // لا تضف /api هنا
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -23,7 +23,7 @@ const useFetch = (url) => {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(`/api${url}`); // إضافة /api إلى بداية المسار
+      const res = await axios.get(url); // لا تضف /api هنا
       setData(res.data);
     } catch (err) {
       setError(err);
